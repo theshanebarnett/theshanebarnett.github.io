62e1cfd7d96678e6e0ffe91558c2fe392f340116
theshanebarnett <theshanebarnett@gmail.com>
2020-02-22 11:17:30 -0600

    Updates

---
+++ precache-manifest.f53c559343cf2acc6f15ba978090bf21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9a7359150810acf006abec0361e45d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1313154ccbefb9b593d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8309dafaf6c01f48f1c4",
+    "url": "/static/js/2.1df98e80.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1df98e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1313154ccbefb9b593d",
+    "url": "/static/js/main.dd8d0499.chunk.js"
+  },
+  {
+    "revision": "39b161432abd32599386",
+    "url": "/static/js/runtime-main.a1acf981.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
